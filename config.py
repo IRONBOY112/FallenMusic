56295744e9cd80925ea41ba381fd070ac064c48b
IRONMAN AND MARCO <iroboy1117@gmail.com>
2025-05-15 14:40:03 +0530

    Update config.py
    Signed-off-by: IRONMAN AND MARCO <iroboy1117@gmail.com>

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGmPPApFlQX919wPp0scooGKuQFm_tHcBMdp8GGaSX2Oihr5L9&s")
+START_IMG = getenv("START_IMG", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHIukuaQi3bjuc4GjT5wfDpcKc00EFW8rPdyXlWbjvrdDb3bvW&s")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ironmanhindigming1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/irotechlab")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7994776745").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGpDLr8L_l3aqmB0ttPm1fXNtLeuultbIihxD6mLcyu0b9YEfD&s"
